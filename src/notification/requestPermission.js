51bb4b0a98eebcffb3d2a0a889c77d65bab1658b
Kegen Guyll <guyllkegen@gmail.com>
2019-08-13 20:06:51 -0500

    able to add multiple tokens to db

--- src/notification/requestPermission.js
+++ src/notification/requestPermission.js
@@ -1,11 +1,45 @@
 import firebase from 'firebase';
+import db from '../credentials/firebaseConfig';
+
+const sendTokenToServer = async currentToken => {
+  let data;
+  const user = firebase.auth().currentUser;
+  const docRef = db.collection('users').doc(user.uid);
+
+  await docRef.get().then(doc => {
+    data = doc.data();
+  });
+
+  if (data.tokens) {
+    console.log('tokens in db');
+    const result = data.tokens.filter(token => token === currentToken);
+    if (result.length === 0) {
+      console.log('adding new token');
+      data.tokens.push(currentToken);
+      docRef.set({ tokens: data.tokens }, { merge: true });
+    } else {
+      console.log('token already exist');
+    }
+  } else {
+    console.log('creating first token');
+    docRef.set({ tokens: [currentToken] }, { merge: true });
+  }
+};
 
 export const askForPermissioToReceiveNotifications = async () => {
   try {
+    let token;
     const messaging = firebase.messaging();
     await messaging.requestPermission();
-    const token = await messaging.getToken();
-    console.log('token:', token);
+    await messaging
+      .getToken()
+      .then(currentToken => {
+        token = currentToken;
+        sendTokenToServer(currentToken);
+      })
+      .then(() => {
+        console.log('token:', token);
+      });
 
     return token;
   } catch (error) {
